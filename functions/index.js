ffb763d7d754d36140a79489c1ad646e72e2efdf
Adolfo <adolfo_zuazo@hotmail.com>
2020-11-06 13:32:26 -0300

    se agrego favicon

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,39 @@
-const functions = require('firebase-functions');
-const nodemailer = require('nodemailer')
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
 
 const transport = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'rodrigomweb@gmail.com',
-        pass: '8wM29lPlQjFSC7mTU'
-    }
-}) 
+  service: "Gmail",
+  auth: {
+    user: "rodrigomweb@gmail.com",
+    pass: "8wM29lPlQjFSC7mTU",
+  },
+});
 
-exports.contactEmail = functions.database.ref('rodrigo-munoz/{id}').onCreate((snap, context) => {
-    const name = snap.val().name
-    const email = snap.val().email
-    const cel = snap.val().cel
-    const message = snap.val().message
-    const marca = snap.val().marca
+exports.contactEmail = functions.database.ref("rodrigo-munoz/{id}").onCreate((snap, context) => {
+  const name = snap.val().name;
+  const email = snap.val().email;
+  const cel = snap.val().cel;
+  const message = snap.val().message;
+  const marca = snap.val().marca;
 
-    return sendContactEmail(name, email, cel, marca, message)
-})
+  return sendContactEmail(name, email, cel, marca, message);
+});
 
 const sendContactEmail = (name, email, cel, marca, message) => {
-    return transport.sendMail({
-        from: name,
-        to: 'ab.rodrigom@gmail.com',
-        subject: 'Mensaje de contacto Web',
-        html:`
+  return transport
+    .sendMail({
+      from: name,
+      to: "ab.rodrigom@gmail.com",
+      subject: "Mensaje de contacto Web",
+      html: `
         <h1>Datos de Interesado</h1>
         <p><strong>Nombre y Apellido:</strong> ${name}</p>
         <p><strong>Cel:</strong> ${cel}</p>
         <p><strong>Marca:</strong> ${marca}</p>
         <p><strong>Producto o servicio:</strong> ${message}</p>
         <p><strong>Email:</strong> ${email}</p>
-    `
+    `,
     })
     .then(r => r)
-    .catch(e => e)
-}
+    .catch(e => e);
+};
--- public/main.js
+++ public/main.js
@@ -1,22 +1,25 @@
-const form = document.getElementById('form')
-const popupBtn = document.getElementById('popupBtn')
-const popupContainer = document.getElementById('popupContainer')
+const form = document.getElementById("form");
+const popupBtn = document.getElementById("popupBtn");
+const popupContainer = document.getElementById("popupContainer");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const name = e.target.name.value
-    const email = e.target.email.value
-    const cel = e.target.cel.value
-    const message = e.target.message.value
-    const marca = e.target.marca.value
-    
-    firebase.database().ref('rodrigo-munoz').push({name, email, cel, marca,  message})
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const cel = e.target.cel.value;
+  const message = e.target.message.value;
+  const marca = e.target.marca.value;
+
+  firebase
+    .database()
+    .ref("rodrigo-munoz")
+    .push({ name, email, cel, marca, message })
     .then(() => {
-        form.reset()
-        popupContainer.classList.add('active')
-    })
-})
+      form.reset();
+      popupContainer.classList.add("active");
+    });
+});
 
-popupBtn.addEventListener('click', () => {
-    popupContainer.classList.remove('active')
-})
+popupBtn.addEventListener("click", () => {
+  popupContainer.classList.remove("active");
+});
